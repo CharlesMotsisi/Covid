8dc574091ae832d97180852ba17af0b35ebcee87
Charles <charlesmotsisi100@gmail.com>
2022-07-22 15:17:17 +0200

    CovidApp

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
+import React, {useEffect,useState} from 'react'
 import logo from './logo.svg';
 import './App.css';
+import GlobalStats from './components/global';
+import CountryStats from './components/countryStats';
+import Search from './components/search';
 
 function App() {
+
+  const [globalStats,setglobalStats] = useState('');
+  const [countrySt,setcountrySt] = useState("");
+
+  useEffect(()=>{
+
+    let url = "https://api.covid19api.com/summary";
+
+    fetch (url).then((response) =>response.json().then((data)=>{
+    console.log(data);
+      setglobalStats(data.Global);
+
+      setcountrySt(data.Countries);
+    }))
+  },[])
+
+  const searchCountry = ((text)=>{
+
+    let value = text.toLowerCase();
+
+    let result = [];
+
+    result = countrySt.filter((countries)=>{
+      return countries.Country.toLowerCase().indexOf(value) !==-1
+    });
+    setcountrySt(result);
+  })
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <GlobalStats global ={globalStats}/>
+      <Search search={searchCountry}/>
+      <CountryStats countries={countrySt}/>
     </div>
   );
 }
---
+++ src/components/countryStats.js
@@ -0,0 +1,25 @@
+
+
+function CountryStats(props){
+    return (
+        <div>
+            <table>
+                <tr>
+                    <th>Country Name</th>
+                    <th>Active Cases</th>
+                </tr>
+
+                {props.countries.map((country)=>(
+                    <tr>
+                        <td>{country.Country}</td>
+                        <td>{country.TotalConfirmed}</td>
+                        <td>{country.NewConfirmed}</td>
+                    </tr>
+                ))}
+            </table>
+
+        </div>
+    )
+}
+
+export default CountryStats;
